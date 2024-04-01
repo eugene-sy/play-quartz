022d55f5713496c2b80cba9de584243dac188c70
Eugene Sypachev (@eugene-sy) <eugene.sypachev@gmail.com>
2024-04-01 20:52:54 +0200

    docs in code

--- src/main/scala/io/github/eugenesy/play/api/quartz/injectable/InjectableJob.scala
+++ src/main/scala/io/github/eugenesy/play/api/quartz/injectable/InjectableJob.scala
@@ -2,4 +2,7 @@ package io.github.eugenesy.play.api.quartz.injectable
 
 import org.quartz.Job
 
+/**
+ * Quartz job interface indicating the requirement to inject dependencies by Guice
+ */
 trait InjectableJob extends Job
--- src/main/scala/io/github/eugenesy/play/api/quartz/injectable/InjectableJobFactory.scala
+++ src/main/scala/io/github/eugenesy/play/api/quartz/injectable/InjectableJobFactory.scala
@@ -7,6 +7,34 @@ import org.quartz.Scheduler
 import org.quartz.simpl.SimpleJobFactory
 import org.quartz.spi.TriggerFiredBundle
 
+/**
+ * Quartz job factory utilizing Guice dependency injection capabilities
+ *
+ * Quartz jobs extending `InjectableJob` interface, provided by he plugin have their fields and setters annotated with
+ * `@Inject` annotation are provided with their dependencies. The jobs extending the default Quartz interface or any
+ * other interfaces, are left as is without injection.
+ *
+ * ===Example===
+ * {{{
+ *    class SimpleJob extends InjectableJob with Logging {
+ *
+ *      @Inject()
+ *      var serviceA: ServiceA = null
+ *
+ *      var serviceB: ServiceB = null
+ *
+ *      @Inject()
+ *      def setServiceB(b: ServiceB) = {
+ *        this.serviceB = b
+ *      }
+ *
+ *      @throws[JobExecutionException]
+ *      override def execute(arg0: JobExecutionContext): Unit = {
+ *        logger.error("This is a quartz job!")
+ *      }
+ *    }
+ * }}}
+ */
 abstract class InjectableJobFactory extends SimpleJobFactory {
 
   def injector: Injector
@@ -23,4 +51,7 @@ abstract class InjectableJobFactory extends SimpleJobFactory {
   }
 }
 
+/**
+ * Default implementation of the InjectableJobFactory
+ */
 class SimpleInjectableJobFactory @Inject() (override val injector: Injector) extends InjectableJobFactory
